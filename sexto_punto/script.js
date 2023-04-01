0e1c7f491c88ceb33fe452825ed94689f5a68136
Sala D7 <CSalaD7@uniboyaca.edu.co>
2023-04-01 11:12:44 -0500

    jquery

---
+++ sexto_punto/script.js
@@ -0,0 +1,13 @@
+function searchText() {
+	var searchText = document.getElementById("searchText").value;
+	var fullText = document.getElementById("fullText").value;
+	
+	var highlightedText = fullText.replace(/<span class="highlight">/gi, "").replace(/<\/span>/gi, "");
+
+	var regex = new RegExp(searchText, "gi");
+	highlightedText = highlightedText.replace(regex, function(match) {
+		return "<span class='highlight'>" + match + "</span>";
+	});
+
+	document.getElementById("fullText").innerHTML = highlightedText;
+}
